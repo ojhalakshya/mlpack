f075a45efb19c5d9bcc3883d0d22a5e66ed5f6f2
ojhalakshya <ojhalakshya@gmail.com>
2020-03-17 10:44:19 +0530

    created pixel_shuffle.hpp and pixle_shuffle_impl.hpp
    changes

---
+++ src/mlpack/methods/ann/layer/pixel_shuffle.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file pixel_shuffle.hpp
+ * @author Lakshya Ojha
+ *
+ * Definition of the Dropout class, which implements a regularizer that
+ * randomly sets units to zero preventing units from co-adapting.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
---
+++ src/mlpack/methods/ann/layer/pixel_shuffle_impl.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file dropout_impl.hpp
+ * @author Marcus Edel
+ *
+ * Implementation of the Dropout class, which implements a regularizer that
+ * randomly sets units to zero. Preventing units from co-adapting.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
